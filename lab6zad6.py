bb82e8d45402502f9a8ed5300313bc560a7e2f77
sa38933 <82753280+sa38933@users.noreply.github.com>
2021-06-12 15:40:12 +0200

    Add files via upload

---
+++ lab6zad6.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 15:19:54 2021
+
+@author: Ania
+"""
+
+def suma(a,b,c):
+    print(a+b+c)
+    
+
+suma(1,6,7)
+suma(2,4,6)
+suma(3,5,9)
+    
---
+++ laboatorium 6 zad5.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 16 14:38:37 2021
+
+@author: Ania
+"""
+
+def przedzial(x):
+    for i in range(-x,x+1):
+        if i%2==0:
+            print(i)
+            
+               
+x=input("Podaj X: ")
+przedzial(int(x))
+            
+            
+            
+
+
+
+            
+        
+        
+        
+        
+        
+
